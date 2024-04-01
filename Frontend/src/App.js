41a68d78490ed54f7da62a4861f3e7796514d523
Gorenpop <144399352+Gorenpop@users.noreply.github.com>
2024-03-31 22:48:23 -0500

    MOdificar Home
    pen

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import LoginForm from './components/LoginForms/LoginForms';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      {/* <Home />  */}
-      <LoginForm/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/LoginForm" element={<LoginForm />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
-}
+};
+
 
 export default App;
--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,51 +1,186 @@
-import React, { useEffect, useState } from 'react';
-import { obtenerDatos, enviarDatos } from '../api/api';
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+  Tooltip,
+  useScrollTrigger,
+  Avatar
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import AdbIcon from '@mui/icons-material/Adb';
+import {useNavigate} from 'react-router-dom';
 
-function Home() {
-  const [datos, setDatos] = useState(null);
+const Home = () => {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+  const navigate = useNavigate();
+  const pages = ['Inicio', 'Productos', 'Servicios', 'Contacto'];
+  const settings = ['Configuración', 'Perfil', 'Cerrar sesión'];
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await obtenerDatos();
-        setDatos(data);
-      } catch (error) {
-        console.error("Error al obtener los datos:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const handleEnviarDatos = async () => {
-    const datosParaEnviar = {
-      "title": "Create Task",
-      "description": "Esta es una tarea de ejemplo para probar el endpoint de creación de tareas.",
-      "complete": true
-    };
-
-    try {
-      const response = await enviarDatos(datosParaEnviar);
-      console.log("Datos enviados con éxito:", response);
-    } catch (error) {
-      console.error("Error al enviar los datos:", error);
-    }
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
   };
 
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+    navigate("/LoginForm");
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
   return (
-    <div>
-      <h1>Conexión React - Django con Axios</h1>
-
-      <button onClick={handleEnviarDatos}>Enviar Datos</button>
-
-      {datos && (
-        <div>
-          <h2>Datos obtenidos:</h2>
-          <pre>{JSON.stringify(datos, null, 2)}</pre>
-        </div>
-      )}
-    </div>
+    <Box>
+      <AppBar
+        position="fixed"
+        style={{
+          width: '100%',
+          top: 0,
+          boxShadow: trigger ? '0px 1px 5px 0px rgba(0, 0, 0, 0.3)' : 'none',
+        }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            {/* Contenido de la barra de navegación */}
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="#home"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              Compostify
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="#home"
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              Compostify
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <div>
+                  <Box sx={{ display: 'flex', alignItems: 'center', margin: '0 10px' }}>
+                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                      <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                    </IconButton>
+                  </Box>
+                </div>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            {/* Añade aquí tu componente ButtonTheme si es necesario */}
+          </Toolbar>
+        </Container>
+      </AppBar>
+      {/* Aquí puedes añadir más contenido para tu componente de inicio */}
+    </Box>
   );
-}
+};
 
 export default Home;
