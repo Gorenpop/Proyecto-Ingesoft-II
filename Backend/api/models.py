283d4c84ed1c278bc9ac92f37d42eaf4a1050fc3
Daniel Felipe Ahumada Hernández <97764610+sadipvp@users.noreply.github.com>
2024-04-01 23:02:21 -0500

    Tablas model.py
    Se agrega las tablas user, order y recolection

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,5 +1,41 @@
 from django.db import models
 
+class User(models.Model):
+    idUser = models.IntegerField()
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=255)
+    first_name = models.CharField(max_length = 255)
+    second_name = models.CharField(max_length=255)
+    isBuyer = models.BooleanField(default = False)
+    isSeller = models.BooleanField(default = False)
+    organization = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    lastSession = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.idUser
+    
+class order(models.Model):
+    idOrder = models.IntegerField()
+    idBuyer = models.ForeignKey(User, on_delete=models.CASCADE)
+    quality = models.CharField(max_length = 255)
+    amount = models.IntegerField()
+    price = models.FloatField()
+    destiny = models.CharField(max_length=255)
+    type = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.idOrder
+
+class recolection(models.Model):
+    idRecolection = models.IntegerField()
+    date = models.DateTimeField()
+    place = models.CharField(max_length = 255)
+    owner = models.CharField(max_length = 255)
+    amount = models.FloatField()
+    idOrder = models.ForeignKey(order, on_delete = models.CASCADE)
+
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
