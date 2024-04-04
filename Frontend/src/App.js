2004852f2e7c9aaa24055604f781c87fd58e046c
CrookedSpecs <saparadaa@unal.edu.co>
2024-04-03 23:21:40 -0500

    se limpio el codigo del navbar, se crearon links rudimentarios para producto, compostar, cuenta, nosotros, login. Se creo una version alpha de la pagina de perfil de usuario y los botones para revisar historial, entre otras cosas

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
-import { Routes, Route } from 'react-router-dom';
 import './App.css';
 //Components
-import Home from './components/pages/Home';
+import Home from './components/pages/Home.tsx';
 import Compostar from './components/Routes/compostar';
 import Nosotros from './components/Routes/nosotros';
 import Productos from './components/Routes/productos';
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/pages/Home.tsx';
->>>>>>> main
 import LoginForm from './components/LoginForms/LoginForms';
 import Cuenta from './components/Routes/cuenta';
+import RegisterForm from './components/RegisterForms/RegisterForm.jsx';
 
 
 const App = () => {
@@ -25,12 +21,8 @@ const App = () => {
         <Route path="/us" element={<Nosotros />} />
         <Route path="/products" element={<Productos />} />
         <Route path="/LoginForm" element={<LoginForm />} />
-<<<<<<< HEAD
-=======
-        <Route path="/Home" element={<Home />} />
         <Route path="/RegisterForm" element={<RegisterForm />} />
->>>>>>> main
-      </Routes>
+      </Routes >
     </div >
   );
 };
